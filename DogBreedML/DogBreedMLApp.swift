99cb80c2fe89f96d570b1e334633a48684be367a
Marc <dyfroedd@gmail.com>
2023-07-03 22:02:40 +0100

    Initial Commit

---
+++ DogBreedML/DogBreedMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedMLApp.swift
+//  DogBreedML
+//
+//  Created by Marc Edwards on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
